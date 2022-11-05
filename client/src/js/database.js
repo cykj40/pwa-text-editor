4aba66f36089798a8a6a390a218bca942403146f
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-11-05 07:38:12 -0400

    installed dependencies

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: 1, content: content});
+  const request = store.put({id: 1, value: content});
 
   const result = await request;
   console.log('🚀 - data saved to the database', result);
